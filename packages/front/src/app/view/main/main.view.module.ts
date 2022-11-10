c0cc24adb0f21b034f4d1a0a21a283fd20ef8d08
Alphaprio <alphaprio@gmail.com>
2022-11-10 10:56:56 +0100

    Squelette HTML de la barre de navigation + component

--- packages/front/src/app/view/main/main.view.module.ts
+++ packages/front/src/app/view/main/main.view.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
+
 import { MainViewComponent } from './component/main.view.component'
+import { NavMainViewComponent } from './nav/component/nav.main.view.component'
 
 @NgModule({
     declarations: [
         MainViewComponent,
+        NavMainViewComponent,
     ],
     imports: [
         CommonModule,
