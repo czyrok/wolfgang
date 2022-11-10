0e814ece50884af6be8388b35fa85c8a748b29e7
Makushimu14 <97458864+Makushimu14@users.noreply.github.com>
2022-11-10 11:46:30 +0100

    Squelette page proposition des cartes HTML + component

---
+++ packages/front/src/app/view/main/cards-proposal/cards-proposal.main.view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-view-main-cards-proposal',
+  templateUrl: './cards-proposal.main.view.component.html',
+  styleUrls: ['./cards-proposal.main.view.component.scss']
+})
+export class CardsProposalMainViewComponent {
+
+}
