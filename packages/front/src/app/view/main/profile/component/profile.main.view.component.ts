931b92c2c1a1e2a2db2c21a700c593c310d45c84
Makushimu14 <97458864+Makushimu14@users.noreply.github.com>
2022-11-14 10:59:14 +0100

    création component profile + création squelette page HTML profile + modification page home

---
+++ packages/front/src/app/view/main/profile/component/profile.main.view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-view-main-profile',
+  templateUrl: './profile.main.view.component.html',
+  styleUrls: ['./profile.main.view.component.scss']
+})
+export class ProfileMainViewComponent {
+
+}
