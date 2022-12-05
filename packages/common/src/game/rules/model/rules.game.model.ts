4a731bdf8828fda1b4996e6ac6b65c5c3a98f678
Makushimu14 <97458864+Makushimu14@users.noreply.github.com>
2022-12-05 16:15:49 +0100

    class rulesGame

---
+++ packages/common/src/game/rules/model/rules.game.model.ts
@@ -0,0 +1,35 @@
+export class RulesGameModel {
+    private _id!: string
+    private _isPrivate!: boolean
+    private _playerCountMax!: number
+
+    public constructor(id: string, isPrivate: boolean, playerCountMax: number) {
+        this._id = id
+        this._isPrivate = isPrivate
+        this._playerCountMax = playerCountMax
+    }
+
+    public set id(value: string) {
+        this._id = value
+    }
+
+    public set isPrivate(value: boolean) {
+        this._isPrivate = value
+    }
+
+    public set playerCountMax(value: number) {
+        this._playerCountMax = value
+    }
+
+    public get id(): string {
+        return this._id
+    }
+
+    public get isPrivate(): boolean {
+        return this._isPrivate
+    }
+
+    public get playerCountMax(): number {
+        return this._playerCountMax
+    }
+}
