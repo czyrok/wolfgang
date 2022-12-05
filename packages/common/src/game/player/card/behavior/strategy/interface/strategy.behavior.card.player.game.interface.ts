2ec233e40f4ecd7f0ffb46179f97c06be187d069
Alphaprio <alphaprio@gmail.com>
2022-12-05 17:37:46 +0100

    Interface ConditionCardBehaviorSrategy

--- packages/common/src/game/player/card/behavior/strategy/interface/strategy.behavior.card.player.game.interface.ts
+++
@@ -1,5 +0,0 @@
-import { ExecuteLoopGameInterface } from "../../../../../loop/execute/interface/execute.loop.game.interface";
-
-export interface StrategyBehaviorCardPlayerGamesIntefrace extends ExecuteLoopGameInterface{
-
-}
