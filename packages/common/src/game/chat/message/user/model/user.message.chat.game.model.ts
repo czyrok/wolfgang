b57cb0864e614b321b4aa9f4a28c6a50ed67615c
Makushimu14 <97458864+Makushimu14@users.noreply.github.com>
2022-12-08 12:02:48 +0100

    ajout classe UserMessageChat

--- packages/common/src/game/chat/message/user/model/user.message.chat.game.model.ts
+++ packages/common/src/game/chat/message/user/model/user.message.chat.game.model.ts
@@ -2,6 +2,7 @@ import { prop, getModelForClass, Ref } from '@typegoose/typegoose'
 import { Exclude, Expose } from 'class-transformer'
 
 import { CollectionName } from '../../../../../decorator/collection-name.decorator'
+import { UserModel } from '../../../../../user/model/user.model'
 import { MessageChatGameModel } from '../../model/message.chat.game.model'
 import { UserMessageChatGameInterface } from '../interface/user.message.chat.game.interface'
 
@@ -11,6 +12,10 @@ export class UserMessageChatGameModel extends MessageChatGameModel implements Us
     @Expose()
     @prop({ required: false, ref: () => UserMessageChatGameModel })
     parent!: Ref<UserMessageChatGameModel>
+
+    @Expose()
+    @prop({ required: true, ref: () => UserModel })
+    user!: Ref<UserModel>
 
 }
 
