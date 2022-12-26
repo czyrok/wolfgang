9472ef0de2029813e273550da110466ee6f3ac01
Dylan <63360083+czyrok@users.noreply.github.com>
2022-12-26 15:28:51 +0100

    Petite correction

--- packages/back-main/src/cosmetic/script/cosmetic.script.ts
+++ packages/back-main/src/cosmetic/script/cosmetic.script.ts
@@ -1,18 +1,21 @@
-import { CosmeticModelDocument } from "../../../../common/src/cosmetic/model/cosmetic.model"
-import { TypeCosmeticEnum } from "../../../../common/src/cosmetic/type/enum/type.cosmetic.enum"
 import { connect } from 'mongoose'
 
-async function insertCosmetic(translateName: string, gamePointPrice: string, imageUrl: string, type: TypeCosmeticEnum ) {
-    await connect("mongodb://localhost:60017/wolfgang", {
-        authSource: "admin",
-        user: "admin",
-        pass: "pass"
-    })
+import { CosmeticModelDocument } from '../../../../common/src/cosmetic/model/cosmetic.model'
+
+import { TypeCosmeticEnum } from '../../../../common/src/cosmetic/type/enum/type.cosmetic.enum'
+
+await connect('mongodb://localhost:60017/wolfgang', {
+    authSource: 'admin',
+    user: 'admin',
+    pass: 'pass'
+})
+
+insertCosmetic('translateName', 'price', 'url', TypeCosmeticEnum.HAT)
+insertCosmetic('translateName', 'price', 'url', TypeCosmeticEnum.HEAD)
+insertCosmetic('translateName', 'price', 'url', TypeCosmeticEnum.TOP)
+insertCosmetic('translateName', 'price', 'url', TypeCosmeticEnum.PANTS)
+insertCosmetic('translateName', 'price', 'url', TypeCosmeticEnum.SHOES)
+
+async function insertCosmetic(translateName: string, gamePointPrice: string, imageUrl: string, type: TypeCosmeticEnum): Promise<void> {
     await new CosmeticModelDocument({ translateName, gamePointPrice, imageUrl, type }).save()
 }
-
-insertCosmetic("translateName", "price", "url", TypeCosmeticEnum.HAT)
-insertCosmetic("translateName", "price", "url", TypeCosmeticEnum.HEAD)
-insertCosmetic("translateName", "price", "url", TypeCosmeticEnum.TOP)
-insertCosmetic("translateName", "price", "url", TypeCosmeticEnum.PANTS)
-insertCosmetic("translateName", "price", "url", TypeCosmeticEnum.SHOES) 
