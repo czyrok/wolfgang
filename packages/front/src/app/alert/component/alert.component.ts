76cfe4c2775f31f3016c4dd39250e0a6af13c557
Makushimu14 <97458864+Makushimu14@users.noreply.github.com>
2022-12-29 10:20:02 +0100

    component abonné au service

--- packages/front/src/app/alert/component/alert.component.ts
+++ packages/front/src/app/alert/component/alert.component.ts
@@ -1,22 +1,25 @@
 import { Component, ViewChild, ViewContainerRef, ComponentRef, HostListener, Input } from '@angular/core'
+
 import { ElementAlertComponent } from '../element/component/element.alert.component'
+import { AlertService } from '../service/alert.service'
 
 @Component({
   selector: 'alert',
   templateUrl: './alert.component.html',
-  styleUrls: ['./alert.component.scss']
+  styleUrls: ['./alert.component.scss'],
+  providers: [AlertService]
 })
 export class AlertComponent {
 
   timeOut!: ReturnType<typeof setTimeout> | undefined
   
   constructor(
-    private componentRef: ComponentRef<AlertComponent>
+    private componentRef: ComponentRef<AlertComponent>,
+    private alertService: AlertService
   ) { }
   
   ngAfterViewInit(): void {
     let alert = new Array<ElementAlertComponent>(this.nbAlert)
-
     alert.forEach( (value) =>{
       value = this.viewContainerRefTarget.createComponent(ElementAlertComponent)
     })
@@ -24,14 +27,14 @@ export class AlertComponent {
     this.timeOut = setTimeout(_ => {
       this.timeOut = undefined
 
-      this.clic(alert)
+      this.clic()
     }, 30000)
   }
   
   @Input() nbAlert!: number
 
   @ViewChild('target', { read: ViewContainerRef }) viewContainerRefTarget!: ViewContainerRef
-  @HostListener('target') clic(alertList: Array<ElementAlertComponent>): void {
+  @HostListener('target') clic(): void {
     this.componentRef.destroy()
     if (this.timeOut !== undefined) clearTimeout(this.timeOut)
   }
