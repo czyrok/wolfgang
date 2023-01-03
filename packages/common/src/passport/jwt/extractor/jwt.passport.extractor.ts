85338dd65273e193003d05e98062ebb38c30f942
Dylan <63360083+czyrok@users.noreply.github.com>
2023-01-03 11:49:12 +0100

    Removing old passport strategy

--- packages/common/src/passport/jwt/extractor/jwt.passport.extractor.ts
+++
@@ -1,7 +0,0 @@
-export class JWTPassportExtractor {
-    public static get(req: any) {
-        if (req.session.token) return req.session.token
-
-        return null
-    }
-}
--- packages/common/src/passport/local/middleware/local.passport.middleware.ts
+++ packages/common/src/passport/local/middleware/local.passport.middleware.ts
@@ -1,11 +1,18 @@
 import { Socket } from 'socket.io'
-import passport from 'passport'
-import { MiddlewareInterface, SocketMiddleware } from 'ts-socket.io-controller'
+import { MiddlewareInterface } from 'ts-socket.io-controller'
+
+import { SocketMiddleware } from '../../../fix/tssocketiocontroller.fix'
+
+import { initialize, authenticate } from '../../../fix/passport.fix'
+
+import { TypePassportEnum } from '../../type/enum/type.passport.enum'
 
 @SocketMiddleware()
 export class LocalPassportMiddleware implements MiddlewareInterface {
-    use(socket: Socket, next: (err?: any) => any) {
-        passport.initialize()(socket.request as any, {} as any, next)
-        passport.authenticate('local')(socket.request as any, {} as any, next)
+    type: 'MiddlewareInterface' = 'MiddlewareInterface'
+
+    use(socket: Socket, next: (err?: any) => any): void {
+        initialize()(socket.request as any, {} as any, next)
+        authenticate(TypePassportEnum.LOCAL)(socket.request as any, {} as any, next)
     }
 }
