35aa90da8560ac681b9891f7cfe895db9f9a9ffc
Makushimu14 <97458864+Makushimu14@users.noreply.github.com>
2023-01-13 16:57:02 +0100

    liaison du front/back des signalements

--- packages/front/src/app/view/managing/report/default/component/default.report.managing.view.component.ts
+++ packages/front/src/app/view/managing/report/default/component/default.report.managing.view.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core'
 
+import { ReportModel } from 'common'
+import { ReceiverEventSocketModel } from 'src/app/socket/event/receiver/model/receiver.event.socket.model'
+import { EventSocketService } from 'src/app/socket/event/service/event.socket.service'
+
 @Component({
   selector: 'app-view-managing-report-default',
   templateUrl: './default.report.managing.view.component.html',
   styleUrls: ['./default.report.managing.view.component.scss']
 })
 export class DefaultReportManagingViewComponent {
+  reportList!: Array<ReportModel>
 
+  reportLink: ReceiverEventSocketModel<Array<ReportModel>> = this.eventSocketLink.registerReceiver<Array<ReportModel>>('/managing/report', 'list').subscribe({
+    callback: (data: Array<ReportModel>) => {
+      this.reportList = data
+    }
+  })
+
+  constructor(
+    private eventSocketLink: EventSocketService
+  ) { }
 }
--- packages/front/src/app/view/managing/report/view/component/view.report.managing.view.component.ts
+++ packages/front/src/app/view/managing/report/view/component/view.report.managing.view.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter, OnInit } from '@angular/core'
+
+import { ActivatedRoute } from '@angular/router'
+
+import { ReportModel } from 'common'
+import { ReceiverEventSocketModel } from 'src/app/socket/event/receiver/model/receiver.event.socket.model'
+import { SenderEventSocketModel } from 'src/app/socket/event/sender/model/sender.event.socket.model'
+import { EventSocketService } from 'src/app/socket/event/service/event.socket.service'
 
 @Component({
   selector: 'app-view-managing-report-view',
   templateUrl: './view.report.managing.view.component.html',
   styleUrls: ['./view.report.managing.view.component.scss']
 })
-export class ViewReportManagingViewComponent {
+export class ViewReportManagingViewComponent implements OnInit {
+  report!: ReportModel
+  
+  reportLink: ReceiverEventSocketModel<ReportModel> = this.eventSocketLink.registerReceiver<ReportModel>('/managing/report', 'view').subscribe({
+    callback: (data: ReportModel) => {
+      this.report = data
+    }
+  })
 
+  idLink: SenderEventSocketModel<string> = this.eventSocketLink.registerSender<string>('/managing/report', 'view')
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private eventSocketLink: EventSocketService
+  ) { }
+
+  ngOnInit(): void {
+    let id: string | null = this.activatedRoute.snapshot.paramMap.get('id')
+
+    if (id !== null) {
+      this.idLink.emit(id)
+    }
+  }
 }
