df580a655b4c9609c2fe58736b54751203876e0b
Dylan <63360083+czyrok@users.noreply.github.com>
2023-01-16 13:31:38 +0100

    Adding the using of sign in/up system

--- packages/front/src/app/view/home/sign-up/component/sign-up.home.view.component.ts
+++ packages/front/src/app/view/home/sign-up/component/sign-up.home.view.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms'
+import { SignUpFormControllerModel, ReceiverLinkSocketModel, SenderLinkSocketModel } from 'common'
+
+import { SocketSharedService } from 'src/app/shared/socket/service/socket.shared.service'
+import { AuthSharedService } from 'src/app/shared/auth/service/auth.shared.service'
+import { DisplayAlertSharedService } from 'src/app/shared/alert/display/service/display.alert.shared.service'
 
 @Component({
   selector: 'app-view-home-sign-up',
@@ -6,5 +13,52 @@ import { Component } from '@angular/core'
   styleUrls: ['./sign-up.home.view.component.scss']
 })
 export class SignUpHomeViewComponent {
+  form: UntypedFormGroup
 
+  constructor(
+    private router: Router,
+    private formBuilder: UntypedFormBuilder,
+    private socketSharedService: SocketSharedService,
+    private authSharedService: AuthSharedService,
+    private displayAlertSharedService: DisplayAlertSharedService
+  ) {
+    this.form = this.formBuilder.group({
+      username: [null, [Validators.minLength(4)]],
+      email: [null, [Validators.email]],
+      password: [null, null]
+    })
+  }
+
+  async onSubmitForm(): Promise<void> {
+    if (this.form.valid) {
+      const tokenLink: ReceiverLinkSocketModel<string>
+        = (await this.socketSharedService.registerReceiver<string>('/home/sign-up', 'trigger'))
+          .subscribe((token: string) => {
+            this.authSharedService.setToken(token)
+
+            this.router.navigateByUrl('/games')
+
+            tokenLink.unsubscribe()
+            errorLink.unsubscribe()
+          })
+
+      const errorLink: ReceiverLinkSocketModel<any>
+        = (await this.socketSharedService.registerReceiver<any>('/home/sign-up', 'trigger-failed'))
+          .subscribe((error: any) => {
+            this.displayAlertSharedService.emitDanger(error)
+
+            tokenLink.unsubscribe()
+            errorLink.unsubscribe()
+          })
+
+      const triggerLink: SenderLinkSocketModel<SignUpFormControllerModel>
+        = await this.socketSharedService.registerSender<SignUpFormControllerModel>('/home/sign-up', 'trigger')
+
+      triggerLink.emit(new SignUpFormControllerModel(
+        this.form.get('username')?.value,
+        this.form.get('email')?.value,
+        this.form.get('password')?.value
+      ))
+    }
+  }
 }
