631a11329887228d2c968c51517ed208161af868
Makushimu14 <97458864+Makushimu14@users.noreply.github.com>
2023-01-16 15:09:55 +0100

    modification component ProfileUserSharedComponent

--- packages/front/src/app/shared/user/line/profile/component/profile.line.user.shared.component.ts
+++ packages/front/src/app/shared/user/line/profile/component/profile.line.user.shared.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { ReceiverLinkSocketModel, SenderLinkSocketModel, UserModel } from 'common'
+import { SocketSharedService } from 'src/app/shared/socket/service/socket.shared.service'
 
 @Component({
   selector: 'app-shared-user-line-profile',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core'
   styleUrls: ['./profile.line.user.shared.component.scss']
 })
 export class ProfileLineUserSharedComponent {
-  
+  user!: UserModel
+
+  async ngAfterViewInit(): Promise<void> {
+
+    let reportLink: ReceiverLinkSocketModel<UserModel> = (await this.eventSocketLink.registerReceiver<UserModel>('/game/profile', 'view')).subscribe(
+      (data: UserModel) => {
+        this.user = data
+        
+      }
+    )
+
+    let usernameLink: SenderLinkSocketModel<string> = await this.eventSocketLink.registerSender<string>('/game/profile', 'view')
+    if (this.username !== null) {
+      usernameLink.emit(this.username)
+    }
+  }
+  constructor(
+    private eventSocketLink: SocketSharedService
+  ) { }
+
+  @Input() username!: string
 }
