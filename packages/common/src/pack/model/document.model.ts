373fadc0406292f5f3178cb0a803facfd9f887f6
Alphaprio <alphaprio@gmail.com>
2023-01-25 17:03:40 +0100

    Finalisation de l'affichage des propositions de cartes

--- packages/common/src/pack/model/document.model.ts
+++ packages/common/src/pack/model/document.model.ts
@@ -3,14 +3,14 @@ import { Expose, Transform } from 'class-transformer'
 export class DocumentModel {
     @Expose()
     @Transform((value) => {
-        if ('value' in value) return value.obj[value.key].toString()
+        if ('value' in value && value.obj[value.key]) return value.obj[value.key].toString()
 
         return '???'
     })
-    protected _id!: string
+    public _id!: string
 
     @Expose()
-    protected __v!: number
+    public __v!: number
 
     public get id(): string {
         return this._id
