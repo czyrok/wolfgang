c5a91a058b49c79a2102aadeee7e0cfde6e76295
Makushimu14 <97458864+Makushimu14@users.noreply.github.com>
2023-01-25 22:33:07 +0100

    Création de la fonction skin dans le controller profile qui renvoie la liste des cosmetic du skin du joueur

--- packages/back-main/src/app/game/profile/controller/profile.game.controller.ts
+++ packages/back-main/src/app/game/profile/controller/profile.game.controller.ts
@@ -1,8 +1,7 @@
 import { DocumentType } from '@typegoose/typegoose'
 import { plainToInstance } from 'class-transformer'
+import { CosmeticModel, CosmeticModelDocument, NotFoundUserError, SkinUserModel, SkinUserModelDocument, UserModel, UserModelDocument } from 'common'
 import { EmitOnFail, EmitOnSuccess, MessageBody, OnConnect, OnDisconnect, OnMessage, SocketController } from 'ts-socket.io-controller'
-import { NotFoundUserError, UserModel, UserModelDocument } from 'common'
-import { LeanDocument } from 'mongoose';
 
 
 @SocketController({
@@ -37,12 +36,94 @@ export class ProfileGameController {
     @EmitOnFail()
     @OnMessage()
     async view(@MessageBody() username: string) {
-        const obj: LeanDocument<UserModel> | null = await UserModelDocument.findOne({
+        const obj = await UserModelDocument.findOne({
             username: username
         }).populate('skin').lean().exec()
 
         if (!obj) throw new NotFoundUserError
 
         return plainToInstance(UserModel, obj)
+    }
+
+    @EmitOnSuccess()
+    @EmitOnFail()
+    @OnMessage()
+    async skin(@MessageBody() username: string) {
+        const user = await UserModelDocument.findOne({
+            username: username
+        }).populate('skin').lean().exec()
+
+        if (!user) throw new NotFoundUserError
+
+        let skin: DocumentType<SkinUserModel> | null = await SkinUserModelDocument.findById(user.skin).exec()
+
+        if (!skin) throw new NotFoundUserError
+
+        const id: string = skin.id
+
+        const cosmeticListObj: Array<DocumentType<CosmeticModel>> = new Array
+        let cosmetic!: DocumentType<CosmeticModel>
+        const cosmeticsListObj: Array<DocumentType<CosmeticModel>> = await CosmeticModelDocument.find().exec()
+
+        for(let oneCosmetic of cosmeticsListObj){
+            cosmetic = new CosmeticModelDocument
+
+            if(oneCosmetic.id === skin.hat.toString()){
+                cosmetic._id = oneCosmetic.id
+                cosmetic.imageUrl = oneCosmetic.imageUrl
+                cosmetic.gamePointPrice = oneCosmetic.gamePointPrice
+                cosmetic.translateName = oneCosmetic.translateName
+                cosmetic.type = oneCosmetic.type
+
+                cosmeticListObj.push(cosmetic)
+            }
+            else if(oneCosmetic.id === skin.head.toString()){
+                cosmetic._id = oneCosmetic.id
+                cosmetic.imageUrl = oneCosmetic.imageUrl
+                cosmetic.gamePointPrice = oneCosmetic.gamePointPrice
+                cosmetic.translateName = oneCosmetic.translateName
+                cosmetic.type = oneCosmetic.type
+
+                cosmeticListObj.push(cosmetic)
+            }
+            else if(oneCosmetic.id === skin.top.toString()){
+                cosmetic._id = oneCosmetic.id
+                cosmetic.imageUrl = oneCosmetic.imageUrl
+                cosmetic.gamePointPrice = oneCosmetic.gamePointPrice
+                cosmetic.translateName = oneCosmetic.translateName
+                cosmetic.type = oneCosmetic.type
+
+                cosmeticListObj.push(cosmetic)
+            }
+            else if(oneCosmetic.id === skin.pants.toString()){
+                cosmetic._id = oneCosmetic.id
+                cosmetic.imageUrl = oneCosmetic.imageUrl
+                cosmetic.gamePointPrice = oneCosmetic.gamePointPrice
+                cosmetic.translateName = oneCosmetic.translateName
+                cosmetic.type = oneCosmetic.type
+
+                cosmeticListObj.push(cosmetic)
+            }
+            else if(oneCosmetic.id === skin.shoes.toString()){
+                cosmetic._id = oneCosmetic.id
+                cosmetic.imageUrl = oneCosmetic.imageUrl
+                cosmetic.gamePointPrice = oneCosmetic.gamePointPrice
+                cosmetic.translateName = oneCosmetic.translateName
+                cosmetic.type = oneCosmetic.type
+
+                cosmeticListObj.push(cosmetic)
+            }
+        }
+
+        const cosmeticList: Array<CosmeticModel> = new Array
+
+        for(let oneCosmetic of cosmeticListObj){
+            const id: string = oneCosmetic.id
+            oneCosmetic = oneCosmetic.toObject()
+            oneCosmetic.id = id
+            cosmeticList.push(oneCosmetic)
+        }
+
+        return cosmeticList
     }
 }
