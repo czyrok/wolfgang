61157cf8df15906825d4b7999964643501dc9242
Dylan <63360083+czyrok@users.noreply.github.com>
2023-02-27 15:18:26 +0100

    Modification des fichiers de conf. pour la VM

--- packages/front/src/environments/environment.prod.ts
+++ packages/front/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   PROTOCOL: 'http',
-  MAIN_URL: '13.94.191.105',
+  MAIN_URL: 'wolfgang.czyrok.ovh',
   MAIN_PORT: 5500,
-  GAME_URL: '13.94.191.105',
+  GAME_URL: 'wolfgang.czyrok.ovh',
   GAME_PORT: 5501,
   SESSION_COOKIE_NAME: 'sid',
   JWT_COOKIE_NAME: 'token',
