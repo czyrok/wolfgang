59d78ce924b2324ad56153b37562aacc9a5cb8d4
Dylan <63360083+czyrok@users.noreply.github.com>
2023-02-28 21:40:35 +0100

    Update cosmetic.script.js

--- packages/back-main/src/cosmetic/script/cosmetic.script.js
+++ packages/back-main/src/cosmetic/script/cosmetic.script.js
@@ -1,34 +1,34 @@
-const mongoose = require('mongoose')
-const common = require('common')
+import { connect } from 'mongoose'
+import { CosmeticModelDocument, CosmeticModel, TypeCosmeticEnum } from 'common'
 
 async function insertCosmetic(translateName, gamePointPrice, imageUrl, type) {
-    await new common.CosmeticModelDocument(new common.CosmeticModel(translateName, gamePointPrice, imageUrl, type)).save()
+    await new CosmeticModelDocument(new CosmeticModel(translateName, gamePointPrice, imageUrl, type)).save()
 }
 
 async function run() {
-    await mongoose.connect('mongodb://localhost:60017/wolfgang', {
+    await connect('mongodb://localhost:60017/wolfgang', {
         authSource: 'admin',
         user: 'admin',
         pass: 'pass'
     })
     
-    insertCosmetic('Chapeau 1', 20, 'hat_1', common.TypeCosmeticEnum.HAT)
-    insertCosmetic('Tête 1', 5, 'head_1', common.TypeCosmeticEnum.HEAD)
-    insertCosmetic('Tee-shirt 1', 20, 'top_1', common.TypeCosmeticEnum.TOP)
-    insertCosmetic('Jean 1', 30, 'pants_1', common.TypeCosmeticEnum.PANTS)
-    insertCosmetic('Chaussures 1', 80, 'shoes_1', common.TypeCosmeticEnum.SHOES)
+    insertCosmetic('Chapeau 1', 20, 'hat_1', TypeCosmeticEnum.HAT)
+    insertCosmetic('Tête 1', 5, 'head_1', TypeCosmeticEnum.HEAD)
+    insertCosmetic('Tee-shirt 1', 20, 'top_1', TypeCosmeticEnum.TOP)
+    insertCosmetic('Jean 1', 30, 'pants_1', TypeCosmeticEnum.PANTS)
+    insertCosmetic('Chaussures 1', 80, 'shoes_1', TypeCosmeticEnum.SHOES)
 
-    insertCosmetic('Chapeau 2', 28, 'hat_2', common.TypeCosmeticEnum.HAT)
-    insertCosmetic('Tête 2', 10, 'head_2', common.TypeCosmeticEnum.HEAD)
-    insertCosmetic('Tee-shirt 2', 23, 'top_2', common.TypeCosmeticEnum.TOP)
-    insertCosmetic('Jean 2', 29, 'pants_2', common.TypeCosmeticEnum.PANTS)
-    insertCosmetic('Chaussures 2', 40, 'shoes_2', common.TypeCosmeticEnum.SHOES)
+    insertCosmetic('Chapeau 2', 28, 'hat_2', TypeCosmeticEnum.HAT)
+    insertCosmetic('Tête 2', 10, 'head_2', TypeCosmeticEnum.HEAD)
+    insertCosmetic('Tee-shirt 2', 23, 'top_2', TypeCosmeticEnum.TOP)
+    insertCosmetic('Jean 2', 29, 'pants_2', TypeCosmeticEnum.PANTS)
+    insertCosmetic('Chaussures 2', 40, 'shoes_2', TypeCosmeticEnum.SHOES)
 
-    insertCosmetic('Chapeau 3', 20, 'hat_3', common.TypeCosmeticEnum.HAT)
-    insertCosmetic('Tête 3', 10, 'head_3', common.TypeCosmeticEnum.HEAD)
-    insertCosmetic('Tee-shirt 3', 27, 'top_3', common.TypeCosmeticEnum.TOP)
-    insertCosmetic('Jean 3', 11, 'pants_3', common.TypeCosmeticEnum.PANTS)
-    insertCosmetic('Chaussures 3', 50, 'shoes_3', common.TypeCosmeticEnum.SHOES)
+    insertCosmetic('Chapeau 3', 20, 'hat_3', TypeCosmeticEnum.HAT)
+    insertCosmetic('Tête 3', 10, 'head_3', TypeCosmeticEnum.HEAD)
+    insertCosmetic('Tee-shirt 3', 27, 'top_3', TypeCosmeticEnum.TOP)
+    insertCosmetic('Jean 3', 11, 'pants_3', TypeCosmeticEnum.PANTS)
+    insertCosmetic('Chaussures 3', 50, 'shoes_3', TypeCosmeticEnum.SHOES)
 }
 
 run()
