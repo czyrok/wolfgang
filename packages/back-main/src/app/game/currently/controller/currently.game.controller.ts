aa6561a9c6ce66501f7277d24a25fff6a9904f74
Dylan <63360083+czyrok@users.noreply.github.com>
2023-03-15 18:10:31 +0100

    Ajout du système d'un nombre maximum de signalement

--- packages/back-main/src/app/game/currently/controller/currently.game.controller.ts
+++ packages/back-main/src/app/game/currently/controller/currently.game.controller.ts
@@ -1,9 +1,9 @@
 import { Namespace, Socket } from 'socket.io'
 import { instanceToPlain } from 'class-transformer'
-import { ConnectedSocket, EmitOnSuccess, OnMessage, SocketController } from 'ts-socket.io-controller'
+import { ConnectedSocket, EmitOnFail, EmitOnSuccess, OnMessage, SocketController } from 'ts-socket.io-controller'
 import { DocumentType } from '@typegoose/typegoose'
 import { Request } from 'express'
-import { GameModel, UserModel, NotFoundUserError } from 'common'
+import { UserHelper, BasicUserReportModelDocument, TooManyTimesReportedUserError, GameModel, UserModel, NotFoundUserError, OtherUserReportModelDocument } from 'common'
 
 import { GetConnectionRegisteryModel } from '../../../registery/connection/get/model/get.connection.registery.model'
 import { CreateConnectionRegisteryModel } from '../../../registery/connection/create/model/create.connection.registery.model'
@@ -25,6 +25,7 @@ export class CurrentlyGameController {
 
     @OnMessage()
     @EmitOnSuccess()
+    @EmitOnFail()
     async create(@ConnectedSocket() socket: Socket) {
         const req: Request = socket.request as Request,
             user: DocumentType<UserModel> | undefined = req.session.user
@@ -34,6 +35,8 @@ export class CurrentlyGameController {
         const gameId: string | null = user.currentGameId
 
         if (gameId) return ''
+
+        if (!(await UserHelper.canJoinOrCreateGame(user))) throw new TooManyTimesReportedUserError
 
         const conn: CreateConnectionRegisteryModel = new CreateConnectionRegisteryModel
 
