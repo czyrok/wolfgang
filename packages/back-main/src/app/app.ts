ca9da506aa3e5d50a64f69c01ea968654e9d19fd
Dylan <63360083+czyrok@users.noreply.github.com>
2023-12-28 15:06:48 +0100

    Correction des configurations pour le déploiement

--- packages/back-main/src/app/app.ts
+++ packages/back-main/src/app/app.ts
@@ -1,78 +1,98 @@
-import { connect } from 'mongoose'
-import { Server } from 'socket.io'
-import { SocketIoController } from 'ts-socket.io-controller'
-import { LogUtil, LogHelper, SessionIoMiddleware, ScopeIoMiddleware, AdminScopeIoMiddleware, ConfigAppHelper, TypeLogEnum, EnvUtil, VarEnvEnum, TestScopeIoMiddleware, PlayScopeIoMiddleware, ReportScopeIoMiddleware } from 'common'
+import { connect } from "mongoose";
+import { Server } from "socket.io";
+import { SocketIoController } from "ts-socket.io-controller";
+import {
+  LogUtil,
+  LogHelper,
+  SessionIoMiddleware,
+  ScopeIoMiddleware,
+  AdminScopeIoMiddleware,
+  ConfigAppHelper,
+  TypeLogEnum,
+  EnvUtil,
+  VarEnvEnum,
+  TestScopeIoMiddleware,
+  PlayScopeIoMiddleware,
+  ReportScopeIoMiddleware,
+} from "common";
 
-import { AuthController } from './auth/controller/auth.controller'
-import { LogInHomeController } from './home/log-in/controller/log-in.home.controller'
-import { SignUpHomeController } from './home/sign-up/controller/sign-up.home.controller'
-import { CurrentlyGameController } from './game/currently/controller/currently.game.controller'
-import { CardsProposalGameController } from './game/cards-proposal/controller/cards-proposal.game.controller'
-import { ViewCardsProposalGameController } from './game/cards-proposal/view/controller/view.cards-proposal.game.controller'
-import { ProfileGameController } from './game/profile/controller/profile.game.controller'
-import { ReportManagingController } from './managing/report/controller/report.managing.controller'
-import { ReportController } from './report/controller/report.controller'
-import { SkinCustomizationProfileGameController } from './game/profile/skin-customization/controller/skin-customization.profile.game.controller'
-import { GameController } from './game/controller/game.controller'
-import { PlayController } from './play/controller/play.controller'
-import { ViewReportManagingController } from './managing/report/view/controller/view.report.managing.controller'
+import { AuthController } from "./auth/controller/auth.controller";
+import { LogInHomeController } from "./home/log-in/controller/log-in.home.controller";
+import { SignUpHomeController } from "./home/sign-up/controller/sign-up.home.controller";
+import { CurrentlyGameController } from "./game/currently/controller/currently.game.controller";
+import { CardsProposalGameController } from "./game/cards-proposal/controller/cards-proposal.game.controller";
+import { ViewCardsProposalGameController } from "./game/cards-proposal/view/controller/view.cards-proposal.game.controller";
+import { ProfileGameController } from "./game/profile/controller/profile.game.controller";
+import { ReportManagingController } from "./managing/report/controller/report.managing.controller";
+import { ReportController } from "./report/controller/report.controller";
+import { SkinCustomizationProfileGameController } from "./game/profile/skin-customization/controller/skin-customization.profile.game.controller";
+import { GameController } from "./game/controller/game.controller";
+import { PlayController } from "./play/controller/play.controller";
+import { ViewReportManagingController } from "./managing/report/view/controller/view.report.managing.controller";
 
 async function run(): Promise<void> {
-    LogUtil.config = LogHelper.getConfig(
-        TypeLogEnum.APP,
-        TypeLogEnum.LOG_IN,
-        TypeLogEnum.ACCESS
-    )
+  LogUtil.config = LogHelper.getConfig(
+    TypeLogEnum.APP,
+    TypeLogEnum.LOG_IN,
+    TypeLogEnum.ACCESS
+  );
 
-    LogUtil.logger(TypeLogEnum.APP).trace('App started')
+  LogUtil.logger(TypeLogEnum.APP).trace("App started");
 
-    await connect(`mongodb://${EnvUtil.get(VarEnvEnum.DB_URL)}:${EnvUtil.get(VarEnvEnum.DB_PORT)}/wolfgang`, {
-        authSource: 'admin',
-        user: EnvUtil.get(VarEnvEnum.DB_USER),
-        pass: EnvUtil.get(VarEnvEnum.DB_PW)
-    })
+  await connect(
+    `mongodb://${EnvUtil.get(VarEnvEnum.DB_URL)}:${EnvUtil.get(
+      VarEnvEnum.DB_PORT
+    )}/wolfgang`,
+    {
+      authSource: "admin",
+      user: EnvUtil.get(VarEnvEnum.DB_USER),
+      pass: EnvUtil.get(VarEnvEnum.DB_PW),
+    }
+  );
 
-    LogUtil.logger(TypeLogEnum.APP).trace('Database connection initialized')
+  LogUtil.logger(TypeLogEnum.APP).trace("Database connection initialized");
 
-    const io: Server = ConfigAppHelper.setup({
-        port: parseInt(EnvUtil.get(VarEnvEnum.MAIN_PORT)),
-        cors: {
-            origin: `${EnvUtil.get(VarEnvEnum.PROTOCOL)}://${EnvUtil.get(VarEnvEnum.CORS_WEBSITE_URL)}:${EnvUtil.get(VarEnvEnum.CORS_WEBSITE_PORT)}`,
-            credentials: true
-        },
-        session: true
-    })
+  const io: Server = ConfigAppHelper.setup({
+    port: parseInt(EnvUtil.get(VarEnvEnum.MAIN_PORT)),
+    cors: {
+      origin: `${EnvUtil.get(VarEnvEnum.PROTOCOL)}://${EnvUtil.get(
+        VarEnvEnum.CORS_WEBSITE_URL
+      )}`,
+      credentials: true,
+    },
+    session: true,
+  });
 
-    SocketIoController.useSocketIoServer(io, {
-        controllers: [
-            AuthController,
-            LogInHomeController,
-            SignUpHomeController,
-            CurrentlyGameController,
-            ProfileGameController,
-            SkinCustomizationProfileGameController,
-            CardsProposalGameController,
-            ViewCardsProposalGameController,
-            ReportManagingController,
-            ReportController,
-            GameController,
-            PlayController,
-            ViewReportManagingController
-        ],
-        middlewares: [
-            SessionIoMiddleware,
-            TestScopeIoMiddleware,
-            ScopeIoMiddleware,
-            AdminScopeIoMiddleware,
-            PlayScopeIoMiddleware,
-            ReportScopeIoMiddleware
-        ],
-        useClassTransformer: true
-    })
+  SocketIoController.useSocketIoServer(io, {
+    controllers: [
+      AuthController,
+      LogInHomeController,
+      SignUpHomeController,
+      CurrentlyGameController,
+      ProfileGameController,
+      SkinCustomizationProfileGameController,
+      CardsProposalGameController,
+      ViewCardsProposalGameController,
+      ReportManagingController,
+      ReportController,
+      GameController,
+      PlayController,
+      ViewReportManagingController,
+    ],
+    middlewares: [
+      SessionIoMiddleware,
+      TestScopeIoMiddleware,
+      ScopeIoMiddleware,
+      AdminScopeIoMiddleware,
+      PlayScopeIoMiddleware,
+      ReportScopeIoMiddleware,
+    ],
+    useClassTransformer: true,
+  });
 
-    LogUtil.logger(TypeLogEnum.APP).trace('Socket engine initialized')
+  LogUtil.logger(TypeLogEnum.APP).trace("Socket engine initialized");
 }
 
 run().catch((error: Error) => {
-    LogUtil.logger(TypeLogEnum.APP).fatal(error.message)
-})
+  LogUtil.logger(TypeLogEnum.APP).fatal(error.message);
+});
--- packages/common/src/pack/app/config/helper/config.app.helper.ts
+++ packages/common/src/pack/app/config/helper/config.app.helper.ts
@@ -1,55 +1,61 @@
-import http from 'http'
-import cors from 'cors'
-import express from 'express'
-import { Server } from 'socket.io'
+import http from "http";
+import cors from "cors";
+import express from "express";
+import { Server } from "socket.io";
 
-import { LogUtil } from '../../../log/util/log.util'
-import { SessionConfigAppUtil } from '../session/util/session.config.app.util'
+import { LogUtil } from "../../../log/util/log.util";
+import { SessionConfigAppUtil } from "../session/util/session.config.app.util";
 
-import { ConfigAppInterface } from '../interface/config.app.interface'
+import { ConfigAppInterface } from "../interface/config.app.interface";
 
-import { TypeLogEnum } from '../../../log/type/enum/type.log.enum'
+import { TypeLogEnum } from "../../../log/type/enum/type.log.enum";
 
 export class ConfigAppHelper {
-    public static setup(options: ConfigAppInterface): Server {
-        const app: express.Application = express(),
-            server: http.Server = http.createServer(app),
-            io = new Server(server, {
-                transports: ['websocket'],
-                cors: options.cors /* {
+  public static setup(options: ConfigAppInterface): Server {
+    const app: express.Application = express(),
+      server: http.Server = http.createServer(app),
+      io = new Server(server, {
+        transports: ["websocket"],
+        cors: options.cors /* {
                     origin: '*',
                     credentials: true
-                } */
-            })
+                } */,
+      });
 
-        app.use(cors(options.cors /* {
+    app.use(
+      cors(
+        options.cors /* {
             origin: true,
             credentials: true
-        } */))
+        } */
+      )
+    );
 
-        /* app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    /* app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
             res.set('Cache-Control', 'no-store')
 
             next()
         }) */
 
-        /* io.engine.on("initial_headers", (headers: any, req: any) => {
+    /* io.engine.on("initial_headers", (headers: any, req: any) => {
             headers['Cache-Control'] = 'no-store'
         }) */
 
-        if (options.session) {
-            app.use(SessionConfigAppUtil.sessionMiddleware)
+    if (options.session) {
+      app.use(SessionConfigAppUtil.sessionMiddleware);
 
-            app.get('/', (req: express.Request, res: express.Response) => {
-                req.session.save()
-                res.status(200).json({})
-            })
-        }
-
-        server.listen(options.port)
-
-        LogUtil.logger(TypeLogEnum.APP).info(`HTTP server listen on port ${options.port}`)
-
-        return io
+      app.get("/", (req: express.Request, res: express.Response) => {
+        req.session.save();
+        res.status(200).json({});
+      });
     }
+
+    server.listen(options.port);
+
+    LogUtil.logger(TypeLogEnum.APP).info(
+      `HTTP server listen on port ${options.port}`
+    );
+
+    return io;
+  }
 }
